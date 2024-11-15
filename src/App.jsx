c78072421e6cd782b4d6501d52ec9f004f77a5fa
trevonakesh <123149383+trevonakesh@users.noreply.github.com>
2024-11-15 23:43:59 +0530

    react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,21 @@
-import React from 'react'
-import './index.css'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'  
-import JobListnings from './components/JobListnings'
-
+import React from "react";
+import "./index.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListnings from "./components/JobListnings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
-
-
-    return (
-      <>
-
-<Navbar/>
-<Hero title="Test title" subtitle="Test subtitle" />
-<HomeCards/>
-<JobListnings/>
-    
-
-
-
-    
-    
-
-   
-
-   
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-      
-      </>
-    
+  return (
+    <>
+      <Navbar />
+      <Hero title="Test title" subtitle="Test subtitle" />
+      <HomeCards />
+      <JobListnings />
+      <ViewAllJobs />
+    </>
   );
-}
+};
 
-export default App
+export default App;
